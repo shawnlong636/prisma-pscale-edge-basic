165f579e78b72708377bad99d5caef6a64e8f0be
Shawn Long <shawnlong636@gmail.com>
2024-03-26 21:36:10 -0700

    fix: added edge runtime in app.d.ts

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -3,13 +3,9 @@ import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
 import { Client } from "@planetscale/database";
 
 import { DATABASE_URL } from "$env/static/private";
-import type { PrismaClientOptions } from "@prisma/client/runtime/library";
 
+const runtime = "edge";
 const client = new Client({ url: process.env.DATABASE_URL });
 const adapter = new PrismaPlanetScale(client);
-
-const options: PrismaClientOptions = {
-    adapter: adapter,
-};
 
 export const prisma = new PrismaClient({ adapter });
