9600c3e83f1edddf45ce37eb8d5d93810e724c09
Emma Long <emma@emmabyte.io>
2024-03-26 21:08:08 -0700

    testing thigns

--- .vercel/output/static/_app/immutable/entry/start.BFOlITCy.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.D2ieUG6M.js";export{t as start};
--- src/lib/database.ts
+++ src/lib/database.ts
@@ -3,7 +3,13 @@ import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
 import { Client } from "@planetscale/database";
 
 import { DATABASE_URL } from "$env/static/private";
+import type { PrismaClientOptions } from "@prisma/client/runtime/library";
 
 const client = new Client({ url: process.env.DATABASE_URL });
 const adapter = new PrismaPlanetScale(client);
+
+const options: PrismaClientOptions = {
+    adapter: adapter,
+};
+
 export const prisma = new PrismaClient({ adapter });
